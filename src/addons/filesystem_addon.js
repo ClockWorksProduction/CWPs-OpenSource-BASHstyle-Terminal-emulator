1efe1672a221c5faa1ca1a1021fabb363193542b
knowledge.zone6869 <knowledge.zone6869@gmail.com>
2025-09-02 07:39:35 +0000

    v4.1.3

---
+++ src/addons/filesystem_addon.js
@@ -0,0 +1,27 @@
+import { Addon } from '../terminal.js';
+
+export class FilesystemAddon extends Addon {
+    constructor() {
+        super('filesystem');
+    }
+
+    onStart(term, vOS) {
+        super.onStart(term, vOS);
+        const readmeContent = `
+Welcome to the CWP Terminal Emulator!
+
+This is a virtual filesystem. Here are some commands you can use:
+
+- **ls**: List files and directories.
+- **cd <directory>**: Change the current directory.
+- **cat <file>**: Display the contents of a file.
+- **pwd**: Show the current working directory.
+- **mkdir <directory>**: Create a new directory.
+- **touch <file>**: Create a new empty file.
+- **tree**: Display the directory structure.
+
+Explore the filesystem and try out the commands!
+`;
+        vOS.createFile('/readme.txt', readmeContent.trim());
+    }
+}
